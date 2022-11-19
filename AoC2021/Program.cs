2b87a7c6f53df8ef198e28a5356ef1b5e1975c48
Freerk de Leeuw <freerk@deleeuw.frl>
2022-11-19 14:25:08 +0100

    AoC 2021 Creation

---
+++ AoC2021/Program.cs
@@ -0,0 +1,12 @@
+﻿using PuzzleSolver.Core;
+
+var puzzleTypes = AppDomain.CurrentDomain.GetAssemblies()
+    .SelectMany(a => a.GetTypes())
+    .Where(t => t.IsSubclassOf(typeof(BasePuzzle)))
+    .ToList();
+
+foreach (var puzzleType in puzzleTypes)
+{
+    var puzzle = (BasePuzzle)Activator.CreateInstance(puzzleType)!;
+    puzzle.Solve();
+}
