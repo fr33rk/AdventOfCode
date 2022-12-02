bfc77c75936e8a8ab86021c96e766cdcac1d7d8f
Freerk de Leeuw <Freerk.de.Leeuw@bd.com>
2022-12-02 08:23:07 +0100

    Puzzle 2

---
+++ AoC2022/Puzzles/Puzzle02.cs
@@ -0,0 +1,141 @@
+using System.ComponentModel;
+using System.Diagnostics;
+using PuzzleSolver.Core;
+
+namespace AoC2022.Puzzles;
+
+public class Puzzle02 : BasePuzzle
+{
+    private enum Shape
+    {
+        Scissor,
+        Paper,
+        Rock
+    }
+
+    private enum Outcome
+    {
+        Win, 
+        Lose,
+        Draw
+    }
+    
+    protected override void SolvePart1(IEnumerable<string> input)
+    {
+        var answer = input.Select(x =>
+            {
+                var round = x.Split(' ');
+                return new
+                {
+                    Oppenent = ToShape(round[0]),
+                    Me = ToShape(round[1])
+                };
+            })
+            .Select(round => Score(round.Oppenent, round.Me))
+            .Sum();
+        
+        Console.WriteLine($"Answer: {answer}");
+    }
+
+    protected override void SolvePart2(IEnumerable<string> input)
+    {
+        var answer = input.Select(x =>
+            {
+                var round = x.Split(' ');
+                return new
+                {
+                    Oppenent = ToShape(round[0]),
+                    Me = FromOutCome(ToShape(round[0]), ToOutcome(round[1]))
+                };
+            })
+            .Select(round => Score(round.Oppenent, round.Me))
+            .Sum();
+        
+        Console.WriteLine($"Answer: {answer}");
+    }
+
+    private static Shape ToShape(string s)
+    {
+        return s switch
+        {
+            "A" or "X" => Shape.Rock,
+            "B" or "Y" => Shape.Paper,
+            "C" or "Z" => Shape.Scissor,
+            _ => throw new ArgumentOutOfRangeException(nameof(s), s, null)
+        };
+    }
+
+    private static Outcome ToOutcome(string s)
+    {
+        return s switch
+        {
+            "X" => Outcome.Lose,
+            "Y" => Outcome.Draw,
+            "Z" => Outcome.Win,
+            _ => throw new ArgumentOutOfRangeException(nameof(s), s, null)
+        };
+    }
+    
+    private static Shape FromOutCome(Shape opponent, Outcome outcome)
+    {
+        return opponent switch
+        {
+            Shape.Paper => outcome switch
+            {
+                Outcome.Win => Shape.Scissor,
+                Outcome.Lose => Shape.Rock,
+                Outcome.Draw => Shape.Paper,
+                _ => throw new ArgumentOutOfRangeException(nameof(outcome), outcome, null)
+            },
+            Shape.Rock => outcome switch
+            {
+                Outcome.Win => Shape.Paper,
+                Outcome.Lose => Shape.Scissor,
+                Outcome.Draw => Shape.Rock,
+                _ => throw new ArgumentOutOfRangeException(nameof(outcome), outcome, null)
+            },
+            Shape.Scissor => outcome switch
+            {
+                Outcome.Win => Shape.Rock,
+                Outcome.Lose => Shape.Paper,
+                Outcome.Draw => Shape.Scissor,
+                _ => throw new ArgumentOutOfRangeException(nameof(outcome), outcome, null)
+            },
+            _ => throw new ArgumentOutOfRangeException(nameof(opponent), opponent, null)
+        };
+    }
+    
+    private static int Score(Shape opponent, Shape me)
+    {
+        var score = me switch
+        {
+            Shape.Scissor => 3,
+            Shape.Paper => 2,
+            Shape.Rock => 1,
+            _ => throw new ArgumentOutOfRangeException(nameof(me), me, null)
+        };
+
+        if (opponent == me)
+        {
+            score += 3;
+        }
+        else if ((opponent == Shape.Paper && me == Shape.Scissor)
+                 || (opponent == Shape.Rock && me == Shape.Paper)
+                 || (opponent == Shape.Scissor && me == Shape.Rock))
+        {
+            score += 6;
+        }
+
+        return score;
+    }
+    
+    protected override IEnumerable<string> GetTestInput()
+    {
+        return new[]
+        {
+            "A Y",
+            "B X",
+            "C Z"
+        };
+    }
+}
